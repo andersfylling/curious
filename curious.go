5074dec74ba42364d7686f0d2b6aa28e3359db3c
Anders Fylling <anders@nordic.email>
2019-06-29 18:02:37 +0200

    github search

---
+++ curious.go
@@ -0,0 +1,95 @@
+package curious
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/go-github/v26/github"
+	"golang.org/x/oauth2"
+	"net/url"
+	"os"
+	"strings"
+)
+
+type ProjectInfo struct {
+	Name   string
+	Author string
+	URL    url.URL
+	Stars  uint
+}
+
+func (p *ProjectInfo) String() string {
+	return p.URL.String()
+}
+
+func GithubSearch(depName string) (projects []*ProjectInfo) {
+	splits := strings.Split(depName, "/")
+	if len(splits) < 3 {
+		panic("must have at least two slashes in a git url to id owner and project name")
+	}
+	source := splits[0]
+	owner := splits[1]
+	project := splits[2]
+
+	fmt.Println("fetching info about", source, owner, project)
+
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("CURIOUS_GITHUB_TOKEN")},
+	)
+	ctx := context.Background()
+	httpClient := oauth2.NewClient(ctx, src)
+	client := github.NewClient(httpClient)
+
+	var results []*github.CodeResult
+	options := github.SearchOptions{}
+	options.PerPage = 100
+	for {
+		result, _, err := client.Search.Code(ctx, depName, &options)
+		if err != nil {
+			panic(err)
+		}
+
+		if len(result.CodeResults) < options.PerPage {
+			break
+		}
+		options.Page++
+
+		for i := range result.CodeResults {
+			if !strings.HasSuffix(*result.CodeResults[i].Path, ".go") {
+				continue
+			}
+			results = append(results, &result.CodeResults[i])
+		}
+	}
+
+	registerredProject := func(u *url.URL) bool {
+		for i := range projects {
+			if projects[i].URL.String() == u.String() {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	for i := range results {
+		r := results[i].Repository
+		usr := r.GetOwner()
+		u, err := url.Parse(r.GetHTMLURL())
+		if err != nil {
+			continue
+		}
+		if registerredProject(u) {
+			continue
+		}
+		if usr != nil && usr.GetName() != owner && r.GetName() != project {
+			projects = append(projects, &ProjectInfo{
+				Name:   r.GetName(),
+				Author: r.GetOwner().GetName(),
+				URL:    *u,
+				Stars:  uint(r.GetStargazersCount()),
+			})
+		}
+	}
+
+	return projects
+}
---
+++ curious_test.go
@@ -0,0 +1,10 @@
+package curious
+
+import (
+	"testing"
+)
+
+func TestGithubSearch(t *testing.T) {
+	//const disgordDep = "github.com/andersfylling/disgord"
+	//fmt.Println(len(GithubSearch(disgordDep)))
+}
